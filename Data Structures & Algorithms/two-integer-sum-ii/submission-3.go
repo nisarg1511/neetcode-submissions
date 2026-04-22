74645daba2d0e2da8f092c987a5652540ba3f455
Joshi Nisarg Devang <127527334+nisarg1511@users.noreply.github.com>
2026-04-22 16:30:54 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.go
@@ -0,0 +1,19 @@
+func twoSum(numbers []int, target int) []int {
+	i,j:=0,len(numbers)-1
+	for i<j{
+		// if numbers[j] >= target{
+		// 	j--
+		// 	continue
+		// }
+		if numbers[i]+numbers[j]==target{
+			return []int{i+1,j+1}
+		}else{
+			if numbers[i]+numbers[j]>target{
+				j--
+			}else {
+				i++
+			}
+		}
+	}
+	return []int{}
+}
