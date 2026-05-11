0414da8fd5e9c9e5a7a063f010df8afb01e49f3f
Joshi Nisarg Devang <127527334+nisarg1511@users.noreply.github.com>
2026-05-11 09:23:34 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	low := 0
+	high :=  len(nums)-1
+	for low <= high{
+		mid := (high + low)/2
+		if nums[mid]==target{
+			return mid
+		}
+		if nums[mid] > target{
+			high = mid-1
+		}else if nums[mid] < target{
+			low = mid+1
+		}
+	}
+	return -1
+}
