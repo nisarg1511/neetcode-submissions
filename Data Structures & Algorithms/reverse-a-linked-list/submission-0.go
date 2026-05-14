78fea2985baa359449c59cd335f1efe15b2a36d6
Joshi Nisarg Devang <127527334+nisarg1511@users.noreply.github.com>
2026-05-14 09:57:03 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	curr:=head
+	var prev *ListNode
+	var next *ListNode
+
+	for curr!=nil{
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+
+		curr = next
+	}
+	return prev
+}
+
